9f4b6c44b42d344a3e7b8c6766dfaf78f8268699
Andrey Konovalov <adech.fo@gmail.com>
2015-07-08 20:40:58 +0300

    Add src/text.rs

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,14 @@ extern crate piston_window;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate freetype;
-extern crate find_folder;
 
 use piston_window::PistonWindow;
 use piston::window::WindowSettings;
 use piston::event::*;
-use opengl_graphics::{ GlGraphics, OpenGL, Texture };
-use graphics::math::Matrix2d;
+use opengl_graphics::{ GlGraphics, OpenGL };
 
-// Taken from here:
-// https://github.com/PistonDevelopers/piston-examples/blob/master/freetype
-fn render_text(face: &mut freetype::Face, gl: &mut GlGraphics, t: Matrix2d, text: &str) {
-    use graphics::*;
-    let mut x = 10;
-    let mut y = 0;
-    for ch in text.chars() {
-        face.load_char(ch as usize, freetype::face::RENDER).unwrap();
-        let g = face.glyph();
-
-        let bitmap = g.bitmap();
-        let texture = Texture::from_memory_alpha(bitmap.buffer(),
-                                                 bitmap.width() as u32,
-                                                 bitmap.rows() as u32).unwrap();
-        Image::new_colored(color::BLACK).draw(
-            &texture,
-            default_draw_state(),
-            t.trans((x + g.bitmap_left()) as f64,
-                    (y - g.bitmap_top()) as f64),
-            gl
-        );
-
-        x += (g.advance().x >> 6) as i32;
-        y += (g.advance().y >> 6) as i32;
-    }
-}
+mod text;
+use text::*;
 
 fn main() {
     let wind_size_x = 900.0;
@@ -59,12 +33,7 @@ fn main() {
         .into();
     let ref mut gl = GlGraphics::new(opengl);
 
-    let assets = find_folder::Search::ParentsThenKids(3, 3)
-        .for_folder("assets").unwrap();
-    let freetype = freetype::Library::init().unwrap();
-    let font = assets.join("Verdana.ttf");
-    let mut face = freetype.new_face(&font, 0).unwrap();
-    face.set_pixel_sizes(0, font_size).unwrap();
+    let mut face = make_face("Verdana.ttf", font_size);
 
     let mut mouse_x = 0.0;
     let mut mouse_y = 0.0;
